79a981bd8aaba869d9f6a96dee76cdee2bdd16d2
cmenseau <cyrielle.menseau@insa-lyon.fr>
2020-07-01 18:07:07 +0200

    modif du test

--- tests/tests/Programs/1_triBulle.c
+++ tests/tests/Programs/1_triBulle.c
@@ -1,7 +1,25 @@
 int main(){
     int tab[] = {18,68,16,65,24,872,6};
     int n=7;
-    tri_a_bulle(tab, n);
+    int non_ordonne = 1;
+    while (non_ordonne){
+        // Supposons le tableau ordonné
+        non_ordonne = 0;
+        int i=0;
+        while (i<n-1){
+            /* Si les 2 éléments sont mal triés */
+            if(tab[i] > tab[i+1]){
+                /* Inversion des 2 éléments */
+                int tmp = tab[i+1];
+                tab[i+1] = tab[i];
+                tab[i] = tmp;
+
+                /* Le tableau n'est toujours pas trié */
+                non_ordonne = 1;
+            }
+            i++;
+        }
+    }
     // est-ce que le tri a fonctionne
     int i=0;
     while (i<n-1){
@@ -13,24 +31,3 @@ int main(){
     return 1;
 }
 
-void tri_a_bulle(int t[], int n){
-    int non_ordonne = 1;
-    while (non_ordonne){
-        // Supposons le tableau ordonné
-        non_ordonne = 0;
-        int i=0;
-        while (i<n-1){
-            /* Si les 2 éléments sont mal triés */
-            if(t[i] > t[i+1]){
-                /* Inversion des 2 éléments */
-                int tmp = t[i+1];
-                t[i+1] = t[i];
-                t[i] = tmp;
-
-                /* Le tableau n'est toujours pas trié */
-                non_ordonne = 1;
-            }
-            i++;
-        }
-    }
-}
